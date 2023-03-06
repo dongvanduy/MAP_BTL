f939332c692e2fd363fe5ece2395d7f9e4e1d044
dong duy <dongduy261299@gmail.com>
2023-03-06 14:05:07 +0700

    Update back signIn

--- app/src/main/java/com/example/quanlysinhvien/SignUpActivity.java
+++ app/src/main/java/com/example/quanlysinhvien/SignUpActivity.java
@@ -7,10 +7,12 @@ import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
@@ -22,6 +24,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class SignUpActivity extends AppCompatActivity {
     private EditText edtEmail, edtPassword;
     private ImageView btnSignUp;
+    private LinearLayout layoutSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +32,14 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
         initUi();
         initListener();
+        initListenerBack();
     }
 
     private void initListener() {
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 onClickSignUp();
             }
         });
@@ -44,6 +49,13 @@ public class SignUpActivity extends AppCompatActivity {
         String strEmail = edtEmail.getText().toString().trim();
         String strPassword = edtPassword.getText().toString().trim();
 
+        if(TextUtils.isEmpty(strEmail)){
+            Toast.makeText(this, "Vui lòng nhập email!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if ((TextUtils.isEmpty(strPassword))){
+            Toast.makeText(this, "Vui lòng nhập Password!", Toast.LENGTH_SHORT).show();
+        }
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         mAuth.createUserWithEmailAndPassword(strEmail, strPassword)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -68,5 +80,16 @@ public class SignUpActivity extends AppCompatActivity {
         edtEmail = findViewById(R.id.edt_email);
         edtPassword = findViewById(R.id.edt_password);
         btnSignUp = findViewById(R.id.btn_sign_up);
+        layoutSignIn = findViewById(R.id.layout_log_in);
+    }
+    private void initListenerBack(){
+        layoutSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
